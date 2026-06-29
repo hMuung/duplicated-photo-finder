e821243ee72e49522711ba70a16fe018db16a6c3
hMuung <hmunguiaestrada@gmail.com>
2026-06-29 18:31:06 -0500

    feat(grayScale): gray scale function logic added

--- lib/PerceptualHash/PHash.cpp
+++ lib/PerceptualHash/PHash.cpp
@@ -4,14 +4,60 @@ PHash::PHash() {}
 
 PHash::~PHash() {}
 
-// Resize image to 32×32
-ImageGrid PHash::resizeImage(const ImageGrid& input) {
+uint64_t PHash::getPHash() {
     
 }
 
-// Convert to grayscale
-ImageGrid PHash::convertToGrayscale(const ImageGrid& input) {
+// Resize image to 32×32
+ImageGrid PHash::resizeImage(const ImageGrid& input) {
+    // Resize Gray scale image to 32x32 size 
+    // using nearest-neighbor interpolation
 
+    const int newSize = 32;
+
+    int oldHeight = input.size();
+    int oldWidth = input[0].size();
+
+    ImageGrid output(newSize, std::vector<float>(newSize));
+
+    for (int y = 0; y < newSize; y++) {
+        for (int x = 0; x < newSize; x++) {
+            int srcY = static_cast<int>((float)y * oldHeight / newSize);
+            int srcX = static_cast<int>((float)x * oldWidth / newSize);
+
+            output[y][x] = input[srcY][srcX];
+        }
+    }
+
+    return output;
+
+}
+
+// Convert to grayscale if gray scale
+ImageGrid PHash::convertToGrayscale(const ImageGrid& input) {
+    // Only return input
+
+    return input;
+}
+
+// Convert to grayscale if color image
+ImageGrid PHash::convertToGrayscale(const ColorImage& input) {
+    // Use standar formula to convert to gray scale
+    // Y = 0.299R + 0.587G + 0.114B
+
+    int height = input.size();
+    int width = input[0].size();
+
+    ImageGrid grayScale(height, std::vector<float>(width));
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            const Pixel& p = input[y][x];
+            grayScale[y][x] = 0.299f*p.r + 0.587f*p.g + 0.114f*p.b;
+        }
+    }
+
+    return grayScale;
 }
 
 // Convert to float
--- lib/PerceptualHash/PHash.h
+++ lib/PerceptualHash/PHash.h
@@ -1,5 +1,18 @@
 #pragma once
 
+/*
+
+Pipeline
+-> grayscale
+-> resize to 32x32
+-> DCT
+-> top-left 8x8
+-> remove DC
+-> median
+-> hash
+
+*/
+
 #include <vector>
 #include <cstdint>
 #include <cmath>
@@ -17,32 +30,42 @@ using ColorImage = std::vector<std::vector<Pixel>>;
 
 class PHash {
     private:
-        // Step 1: Resize image to 32×32
+        // Resize image to 32×32
+        ImageGrid readImage(std::string_view path);
+        ColorImage readImage(std::string_view path);
+
+        // Resize image to 32×32
         ImageGrid resizeImage(const ImageGrid& input);
 
-        // Step 2: Convert to grayscale
+        // Convert to grayscale if gray scale
         ImageGrid convertToGrayscale(const ImageGrid& input);
 
-        // Step 3: Convert to float
+        // Convert to grayscale if color image
+        ImageGrid convertToGrayscale(const ColorImage& input);
+
+        // Convert to float
         ImageGrid convertToFloat(const ImageGrid& input);
 
-        // Step 4: Apply DCT
+        // Apply Descrete Cosine Transform
         ImageGrid applyDCT(const ImageGrid& input);
 
-        // Step 5: Keep top-left 8×8 block
+        // Keep top-left 8×8 block
         ImageGrid extractTopLeft8x8(const ImageGrid& dctMat);
 
-        // Step 6: Ignore the DC coefficient
+        // Ignore the DC coefficient
         std::vector<float> flattenAndIgnoreDC(const ImageGrid& block8x8);
 
-        // Step 7: Compute median
+        // Compute median
         float computeMedian(std::vector<float> values);
 
-        // Step 8: Generate binary hash
+        // Generate binary hash
         uint64_t generateBinaryHash(const std::vector<float>& coefficients, float median);
 
     public:
         PHash();
         ~PHash();
+
+        uint64_t getPHash();
+
 };
 
